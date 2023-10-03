10ebf08b9e393a4ef5528d035a7544a1c4710b71
Ciro Artigot <ciro.artigot@gmail.com>
2023-10-03 18:36:38 +0200

    Commits 031023

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import {client} from '@/utils/sanity/client'
-import { Post } from '@/lib/sanity.queries'
+
 
 // `app/page.tsx` is the UI for the `/` URL
 export default async function Page() {
 
-  const posts = await client.fetch<Post[]>(`*[_type == "post"]`)
-
-  console.log(posts)
 
   return <h1>Hello, Home page!</h1>
 } 
